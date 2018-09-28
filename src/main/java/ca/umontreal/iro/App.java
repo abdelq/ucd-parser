3033a6156923b7120e61e6c9f4d40f854e7ff256
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-09-28 19:31:00 -0400

    New Maven project

---
+++ src/main/java/ca/umontreal/iro/App.java
@@ -0,0 +1,13 @@
+package ca.umontreal.iro;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
