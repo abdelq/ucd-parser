893de45afe74eaaed6fd3a6672e220025f7e546a
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-09-29 23:23:34 -0400

    Template for a GUI with a simple menu in OpenJFX

--- src/main/java/ca/umontreal/iro/App.java
+++ src/main/java/ca/umontreal/iro/App.java
@@ -1,13 +1,54 @@
 package ca.umontreal.iro;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
+import java.io.File;
+
+import javafx.application.Application;
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.scene.Scene;
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuBar;
+import javafx.scene.control.MenuItem;
+import javafx.scene.layout.VBox;
+import javafx.stage.FileChooser;
+import javafx.stage.Stage;
+
+public class App extends Application {
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        var box = new VBox();
+        var scene = new Scene(box, 640, 480);
+
+        var menuBar = new MenuBar();
+        var file = new Menu("File");
+
+        var open = new MenuItem("Open");
+        var fileChooser = new FileChooser();
+        open.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(final ActionEvent e) {
+                File file = fileChooser.showOpenDialog(stage);
+                if (file != null) {
+                    // TODO
+                }
+            }
+        });
+
+        var exit = new MenuItem("Exit");
+        exit.setOnAction((event) -> {
+            System.exit(0);
+        });
+
+        file.getItems().addAll(open, exit);
+        menuBar.getMenus().addAll(file);
+        box.getChildren().addAll(menuBar);
+
+        stage.setTitle("UCD Parser");
+        stage.setScene(scene);
+        stage.show();
     }
 }
