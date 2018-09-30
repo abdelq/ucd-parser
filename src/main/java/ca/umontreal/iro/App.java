56c20b4e3949e0fba9deeb2417dbc78508c522f4
Abdelhakim Qbaich <abdelhakim@qbaich.com>
2018-09-30 11:26:52 -0400

    Separating the creation of the menu from the App

--- src/main/java/ca/umontreal/iro/App.java
+++ src/main/java/ca/umontreal/iro/App.java
@@ -1,16 +1,9 @@
 package ca.umontreal.iro;
 
-import java.io.File;
-
+import ca.umontreal.iro.ui.MenuBar;
 import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
-import javafx.scene.control.Menu;
-import javafx.scene.control.MenuBar;
-import javafx.scene.control.MenuItem;
 import javafx.scene.layout.VBox;
-import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 public class App extends Application {
@@ -19,36 +12,11 @@ public class App extends Application {
     }
 
     @Override
-    public void start(Stage stage) throws Exception {
-        var box = new VBox();
-        var scene = new Scene(box, 640, 480);
-
-        var menuBar = new MenuBar();
-        var file = new Menu("File");
-
-        var open = new MenuItem("Open");
-        var fileChooser = new FileChooser();
-        open.setOnAction(new EventHandler<ActionEvent>() {
-            @Override
-            public void handle(final ActionEvent e) {
-                File file = fileChooser.showOpenDialog(stage);
-                if (file != null) {
-                    // TODO
-                }
-            }
-        });
-
-        var exit = new MenuItem("Exit");
-        exit.setOnAction((event) -> {
-            System.exit(0);
-        });
-
-        file.getItems().addAll(open, exit);
-        menuBar.getMenus().addAll(file);
-        box.getChildren().addAll(menuBar);
+    public void start(Stage stage) {
+        var vBox = new VBox(new MenuBar(stage));
 
         stage.setTitle("UCD Parser");
-        stage.setScene(scene);
+        stage.setScene(new Scene(vBox, 640, 480));
         stage.show();
     }
 }
---
+++ src/main/java/ca/umontreal/iro/ui/MenuBar.java
@@ -0,0 +1,36 @@
+package ca.umontreal.iro.ui;
+
+import javafx.scene.control.Menu;
+import javafx.scene.control.MenuItem;
+import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Stage;
+
+import java.io.File;
+
+public class MenuBar extends javafx.scene.control.MenuBar {
+    public MenuBar(Stage stage) {
+        var fileMenu = new Menu("File");
+
+        var fileChooser = new FileChooser();
+        fileChooser.setTitle("Open File");
+        fileChooser.getExtensionFilters().addAll(
+            new ExtensionFilter("UCD Files", "*.ucd"),
+            new ExtensionFilter("All Files", "*.*")
+        );
+
+        var open = new MenuItem("Open");
+        open.setOnAction(event -> {
+            File file = fileChooser.showOpenDialog(stage);
+            if (file != null) {
+                // TODO
+            }
+        });
+
+        var exit = new MenuItem("Exit");
+        exit.setOnAction(event -> System.exit(0));
+
+        fileMenu.getItems().addAll(open, exit);
+        this.getMenus().addAll(fileMenu);
+    }
+}
